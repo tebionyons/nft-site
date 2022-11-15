300959561ceecbae1a711ab71f9c02b15a439912
Tebi Remion <tembeiremion@gmail.com>
2022-11-15 23:22:09 +0100

    Fix modal

--- src/components/placetostay/PlaceToStay.jsx
+++ src/components/placetostay/PlaceToStay.jsx
@@ -1,49 +1,71 @@
-import { Footer, Navbar } from 'components';
-import PlaceCard from 'components/placecard/PlaceCard';
-import React from 'react';
-import './placetostay.css';
-import PlaceData from 'PlaceData';
-import Modal from 'components/modal/Modal';
+import { Footer, Navbar } from "components";
+import PlaceCard from "components/placecard/PlaceCard";
+import React from "react";
+import "./placetostay.css";
+import PlaceData from "PlaceData";
+import Modal from "components/modal/Modal";
 
 const PlaceToStay = () => {
-    const place = PlaceData.map(cards=>{
-        return <PlaceCard 
-        
+  const place = PlaceData.map((cards) => {
+    return (
+      <PlaceCard
         title={cards.title}
         img={cards.img}
         distance={cards.distance}
         time={cards.time}
-        availability={cards.availability} />
-    })
+        availability={cards.availability}
+      />
+    );
+  });
   return (
     <>
-    <div>
+      <div>
         <Navbar />
-        
-        <div className='second__nav'>
-            <p><a href='#home'>Restaurant</a></p>
-            <p><a href='#home'>Cottage</a></p>
-            <p><a href='#home'>castle</a></p>
-            <p><a href='#home'>fantast city</a></p>
-            <p><a href='#home'>beach</a></p>
-            <p><a href='#home'>Carbins</a></p>
-            <p><a href='#home'>Off-grid</a></p>
-            <p><a href='#home'>Farm</a></p>
-            <img className='locaation__img' src='./images/location.png' alt='location'/>
-            
-        </div>
-      <div className='card__list'>
-      {place}
-      <div className='footer__'>
-      <Footer/>
-      </div>
-      
-        </div>  
-        
-    </div>
-   
-    </>
-  )
-}
 
-export default PlaceToStay
+        <div className="second__nav">
+          <p>
+            <a href="#home">Restaurant</a>
+          </p>
+          <p>
+            <a href="#home">Cottage</a>
+          </p>
+          <p>
+            <a href="#home">castle</a>
+          </p>
+          <p>
+            <a href="#home">fantast city</a>
+          </p>
+          
+            {" "}
+            <p>
+              <a href="#home">beach</a>
+            </p>
+            <p>
+              <a href="#home">Carbins</a>
+            </p>
+            <p>
+              <a href="#home">Off-grid</a>
+            </p>
+            <p>
+              <a href="#home">Farm</a>
+            </p>
+          
+
+          <img
+            className="locaation__img"
+            src="./images/location.png"
+            alt="location"
+          />
+        </div>
+        <div className="card__list">
+          {place}
+          <div className="footer__">
+            <Footer />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PlaceToStay;
