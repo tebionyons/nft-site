408cfd63c379484cc5bf8f23f2170b0e70206786
Tebi Remion <tembeiremion@gmail.com>
2022-11-16 11:51:01 +0100

    Bug fix

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,15 @@ import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Modal from "components/modal/Modal";
-import { click } from "@testing-library/user-event/dist/click";
+
 
 const Navbar = ({ clicked, isClicked }) => {
   const [openModal, setOpenModal] = useState(false);
+  const [menu, setMenu] = useState(false)
 
   const handleClicked = () => {
-    isClicked(!clicked);
+    setMenu(!menu);
+    
   };
   return (
     <div className="nft__navbar">
@@ -19,7 +21,7 @@ const Navbar = ({ clicked, isClicked }) => {
           <img src="./images/bnb.png" alt="#logo" />
           <img src="./images/logo.png" alt="#textlogo" />
         </div>
-        <div className="nft__navbar-links-container">
+        <div className={`${!menu ? 'display nft__navbar-links-container': "nft__navbar-links-container"}`}>
           <p>
             <Link to="/">Home</Link>
           </p>
@@ -46,10 +48,10 @@ const Navbar = ({ clicked, isClicked }) => {
           </button>
           {openModal && <Modal closeModal={setOpenModal} />}
         </div>
-        {!clicked ? (
-          <RiMenu3Line onClick={handleClicked} className="icon" />
+        {!menu ? (
+          <RiMenu3Line onClick={()=>handleClicked()} className="icon" />
         ): (
-          <RiCloseLine onClick={handleClicked} className="icon"/>
+          <RiCloseLine onClick={()=>handleClicked()} className="icon"/>
         )}
       </div>
     </div>
